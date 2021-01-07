bbf24bb2227a82336f064c2324d2572d6839b8b6
gabriel3421 <gabrielsouza.n.s@hotmail.com>
2021-01-07 18:49:20 -0300

    agenda do prestador

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import ensureAuthenticated from '@modules/users/infra/http/middleware/ensureAuthenticated';
 import AppointmentsController from '../controllers/AppointmentsController';
+import ProviderAppointmentsController from '../controllers/ProvidersAppointmetController';
 
 const appointmentsRouter = Router();
 const appointmentsController = new AppointmentsController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 appointmentsRouter.use(ensureAuthenticated);
 // appointmentsRouter.get('/', async (req, res) => {
 //   const appointments = await appointmentRepository.find();
@@ -11,4 +13,5 @@ appointmentsRouter.use(ensureAuthenticated);
 // });
 
 appointmentsRouter.post('/', appointmentsController.create);
+appointmentsRouter.get('/me', providerAppointmentsController.index);
 export default appointmentsRouter;
