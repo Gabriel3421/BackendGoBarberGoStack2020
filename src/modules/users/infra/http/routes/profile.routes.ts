be5cff8a901ef200ebd35686b45db0daaee13b6b
gabriel3421 <gabrielsouza.n.s@hotmail.com>
2021-01-09 21:29:09 -0300

    validando dados da rota

--- src/modules/users/infra/http/routes/profile.routes.ts
+++ src/modules/users/infra/http/routes/profile.routes.ts
@@ -1,11 +1,24 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 import ProfileController from '../controllers/ProfileController';
 import ensureAuthenticated from '../middleware/ensureAuthenticated';
 
 const profileRouter = Router();
 const profileController = new ProfileController();
 profileRouter.use(ensureAuthenticated);
-profileRouter.put('/', profileController.update);
+profileRouter.put(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required(),
+      name: Joi.string().required(),
+      old_password: Joi.string(),
+      password: Joi.string(),
+      password_confirmation: Joi.string().valid(Joi.ref('password')),
+    },
+  }),
+  profileController.update,
+);
 profileRouter.get('/', profileController.show);
 
 export default profileRouter;
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import express, { Request, Response, NextFunction, response } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
+import { errors } from 'celebrate';
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
 import routes from './routes';
@@ -13,6 +14,9 @@ app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
+
+app.use(errors());
+
 app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
   if (err instanceof AppError) {
     return response.status(err.statusCode).json({
