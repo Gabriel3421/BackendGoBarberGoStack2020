6b49ce4e608b2aa73cd9a49e2918fdd096b5c175
gabriel3421 <gabrielsouza.n.s@hotmail.com>
2021-02-18 14:33:01 -0300

    ajustes para adptar o frontend

--- src/modules/appointments/infra/http/controllers/DayAvailabilityController.ts
+++ src/modules/appointments/infra/http/controllers/DayAvailabilityController.ts
@@ -5,7 +5,7 @@ import ListProviderDayAvailabilityService from '@modules/appointments/services/L
 export default class MonthAvailabilityController {
   public async index(request: Request, response: Response): Promise<Response> {
     const { provider_id } = request.params;
-    const { day, month, year } = request.body;
+    const { day, month, year } = request.query;
 
     const listProviderDayAvailabilityService = container.resolve(
       ListProviderDayAvailabilityService,
@@ -13,9 +13,9 @@ export default class MonthAvailabilityController {
 
     const availability = await listProviderDayAvailabilityService.execute({
       provider_id,
-      month,
-      year,
-      day,
+      month: Number(month),
+      year: Number(year),
+      day: Number(day),
     });
     return response.json(availability);
   }
--- src/modules/appointments/infra/http/controllers/MonthAvailabilityController.ts
+++ src/modules/appointments/infra/http/controllers/MonthAvailabilityController.ts
@@ -5,7 +5,7 @@ import ListMonthAvailabilityService from '@modules/appointments/services/ListMon
 export default class MonthAvailabilityController {
   public async index(request: Request, response: Response): Promise<Response> {
     const { provider_id } = request.params;
-    const { month, year } = request.body;
+    const { month, year } = request.query;
 
     const listMonthAvailabilityService = container.resolve(
       ListMonthAvailabilityService,
@@ -13,8 +13,8 @@ export default class MonthAvailabilityController {
 
     const availability = await listMonthAvailabilityService.execute({
       provider_id,
-      month,
-      year,
+      month: Number(month),
+      year: Number(year),
     });
     return response.json(availability);
   }
